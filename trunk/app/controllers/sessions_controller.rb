6590d3e4f35204bac296705cf7e91ff223423bb1
frankie@frankieroberto.com <frankie@frankieroberto.com@8e0ef8be-9d4e-0410-a7b5-71e0103ca829>
2008-07-12 13:47:39 +0000

    Getting rid of annoying flash message

--- trunk/app/controllers/sessions_controller.rb
+++ trunk/app/controllers/sessions_controller.rb
@@ -11,7 +11,6 @@ class SessionsController < ApplicationController
         cookies[:auth_token] = { :value => self.current_user.remember_token , :expires => self.current_user.remember_token_expires_at }
       end
       redirect_back_or_default('/')
-      flash[:notice] = "Logged in successfully"
     else
       render :action => 'new'
     end
