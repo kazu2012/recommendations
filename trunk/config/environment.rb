b1a682c99e57b3a14e50d7817143c775a961a222
frankie@frankieroberto.com <frankie@frankieroberto.com@8e0ef8be-9d4e-0410-a7b5-71e0103ca829>
2008-08-05 16:40:59 +0000

    Improvement to error valiation html/css

--- trunk/config/environment.rb
+++ trunk/config/environment.rb
@@ -64,4 +64,18 @@ Rails::Initializer.run do |config|
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
+
+end
+
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  error_class = "error"
+  if html_tag =~ /<(input|textarea|select)[^>]+class=/
+    class_attribute = html_tag =~ /class=['"]/
+    html_tag.insert(class_attribute + 7, "#{error_class} ")
+  elsif html_tag =~ /<(input|textarea|select)/
+    first_whitespace = html_tag =~ /\s/
+    html_tag[first_whitespace] = " class='#{error_class}' "
+  end
+  html_tag
 end
